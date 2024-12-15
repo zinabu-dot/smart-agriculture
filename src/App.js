8b721abc392d70ed05896539791562007fe47da3
zinabu-dot <take.z70@gmail.com>
2024-12-15 01:49:56 +0100

    fixing routr

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/SmartAgri">
+    <Router basename="/smart-agriculture">
       <div className="App">
         <Navbar />
         <Routes>
